74f9077e53c514618881bbf8de31ff53cacdda64
Arul D U <uaruld@gmail.com>
2025-10-11 09:57:57 +0000

    kenapa pas pengisian tanggal lahir tidak bisa memilih tahun

--- src/app/(main)/membership/register/page.tsx
+++ src/app/(main)/membership/register/page.tsx
@@ -123,6 +123,9 @@ export default function RegisterPage() {
                             <PopoverContent className="w-auto p-0" align="start">
                                 <Calendar
                                     mode="single"
+                                    captionLayout="dropdown-buttons"
+                                    fromYear={1900}
+                                    toYear={new Date().getFullYear()}
                                     selected={field.value}
                                     onSelect={field.onChange}
                                     disabled={(date) =>
